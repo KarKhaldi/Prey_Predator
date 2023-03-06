17fa416dc807839d2a0601c7e440f829535018ee
leilab98 <leilaberrada@MacBook-Air-de-Leila.local>
2023-03-06 19:54:40 +0100

    add images

--- prey_predator/server.py
+++ prey_predator/server.py
@@ -4,28 +4,32 @@ from mesa.visualization.UserParam import UserSettableParameter
 
 from prey_predator.agents import Wolf, Sheep, GrassPatch
 from prey_predator.model import WolfSheep
+import os
 
 
 def wolf_sheep_portrayal(agent):
     if agent is None:
         return
-    portrayal = {}
-    portrayal["Shape"]="circle"
-    portrayal["Filled"] ="true"
-    portrayal["Layer"] = 0
-    portrayal["r"]= 0.5
+    portrayal = {"Shape": "circle",
+                               "Filled": "true",
+                               "Layer": 0,
+                               "r": 0.5}
+    # portrayal["Shape"]="circle"
+    # portrayal["Filled"] ="true"
+    # portrayal["Layer"] = 3
+    # portrayal["r"]= 0.5
     
     if type(agent) is Sheep:
-        portrayal["Color"]= "grey"
+        portrayal["Shape"] = f"{os.path.dirname(os.path.realpath(__file__))}/sheep.png",
         # ... to be completed
     elif type(agent) is Wolf:
-        # ... to be completed
-        portrayal["Color"]= "red"
+        portrayal["Shape"] = f"{os.path.dirname(os.path.realpath(__file__))}/wolf.png",
+
     elif type(agent) is GrassPatch:
         if agent.grown ==True:
-            portrayal["Color"]= "green"
+            portrayal["Shape"] = f"{os.path.dirname(os.path.realpath(__file__))}/grass.jpeg",
         else:
-            portrayal["Color"]= "white"
+            portrayal["Shape"] = f"{os.path.dirname(os.path.realpath(__file__))}/death_grass.jpg",
         # ... to be completed
     return portrayal
 
@@ -42,4 +46,4 @@ model_params = {
 server = ModularServer(
     WolfSheep, [canvas_element, chart_element], "Prey Predator Model", model_params
 )
-server.port = 8521
+server.port = 8525
